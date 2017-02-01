dd31a3376193bb8d66d2111f26d7874b5995d167
Danny Tunon <dtunon@osisoft.com>
2017-02-01 19:39:21 +0000

    Add panels prop (content children) and afterContentInit

--- src/app/panel-container/panel-container.component.ts
+++ src/app/panel-container/panel-container.component.ts
@@ -1,15 +1,28 @@
-import { Component, Input, HostBinding, OnInit } from '@angular/core';
+import
+  { Component,
+    Input,
+    HostBinding,
+    ContentChildren,
+    QueryList,
+    ElementRef,
+    AfterContentInit,
+  } from '@angular/core';
+
+import { PanelComponent } from './../panel/panel.component';
 
 @Component({
   selector: 'panel-container',
   templateUrl: './panel-container.component.html',
   styleUrls: ['./panel-container.component.css']
 })
-export class PanelContainerComponent implements OnInit {
+export class PanelContainerComponent implements AfterContentInit {
   @HostBinding('style.flexDirection') @Input() direction = 'row';
+
+  @ContentChildren(PanelComponent) panels: QueryList<PanelComponent>;
+
   constructor() { }
 
-  ngOnInit() {
+  ngAfterContentInit() {
   }
 
 }
