200cb7eb9fedd4f9027ae35d0e7ffdce9a17acc7
Danny Tunon <danieltunon@gmail.com>
2017-02-07 22:12:46 +0000

    Apply direction as class; begin work on window resize

--- src/app/dual-panel/dual-panel.component.ts
+++ src/app/dual-panel/dual-panel.component.ts
@@ -18,12 +18,14 @@ import { Subject } from 'rxjs/Subject'
   styleUrls: ['./dual-panel.component.less'],
 })
 export class DualPanelComponent implements OnInit, OnDestroy {
-  @Input() direction: string = 'row';
-  // @HostBinding('style.flexDirection') @Input() direction: string = 'row';
+  // @Input() direction: string = 'row';
+  @HostBinding('class') @Input() direction: string = 'row';
   @Input() name: string;
   @HostListener('mousemove', ['$event']) onmousemove(e: MouseEvent) {
-    e.stopPropagation();
-    // this.test2$.next(e);
+    // stop propagation was preventing the mousemove event from firing when the mouse moved
+    // into another panel, but checking if this.isResizing is sufficient since only the panel
+    // that owns the splitter being dragged will have that toggeled on
+    e.preventDefault();
     if (this.isResizing) this.update(e);
   }
   @HostListener('mouseup', ['$event']) onmouseup(e: MouseEvent) {
@@ -31,14 +33,14 @@ export class DualPanelComponent implements OnInit, OnDestroy {
   }
   @ViewChild('splitter') splitter: ElementRef;
 
-  private test$: Subject<any> = new Subject();
-  private test2$: Subject<any> = new Subject();
-
+  // private test$: Subject<any> = new Subject();
+  // private test2$: Subject<any> = new Subject();
   private isResizing: boolean = false;
   startCoord: number;
   containerSize: number;
   firstPanelSize: number = 250;
   secondPanelSize: number = 250;
+  dimension: string;
 
 
   constructor(private el: ElementRef) { }
@@ -104,17 +106,17 @@ export class DualPanelComponent implements OnInit, OnDestroy {
   //   console.log(this.name, 'oc', size, conts)
   // }
   ngOnInit()              {
-    let size = this.direction === 'row' ? 'width' : 'height';
-    let conts = this.el.nativeElement.getBoundingClientRect()[size] -
-                this.splitter.nativeElement.getBoundingClientRect()[size];
-    console.log(this.name, 'oi', size, conts);
-    this.firstPanelSize = ~~(conts / 2);
-    this.secondPanelSize = ~~(conts / 2);
+    this.dimension = this.direction === 'row' ? 'width' : 'height';
+    this.containerSize = this.el.nativeElement.getBoundingClientRect()[this.dimension] - 4; // splitter not defined yet;
+    console.log(this.name, 'oi', this.dimension, this.containerSize);
+    this.firstPanelSize = this.containerSize / 2;
+    this.secondPanelSize = this.containerSize / 2;
   }
   // ngDoCheck()             {
   //   let size = this.direction === 'row' ? 'width' : 'height';
-  //   let conts = this.el.nativeElement.getBoundingClientRect()[size];
-  //   console.log(this.name, 'dc', size, conts)
+  //   if (this.containerSize !== this.el.nativeElement.getBoundingClientRect()[size] - 4) {
+  //     window.setTimeout(this.onResize.bind(this));
+  //   }
   // }
   // ngAfterContentInit()    {
   //   let size = this.direction === 'row' ? 'width' : 'height';
@@ -132,12 +134,22 @@ export class DualPanelComponent implements OnInit, OnDestroy {
     let size = this.direction === 'row' ? 'width' : 'height';
     let conts = this.el.nativeElement.getBoundingClientRect()[size] -
                 this.splitter.nativeElement.getBoundingClientRect()[size];
-    console.log(this.name, 'avi', size, conts)
+    let other = this.direction !== 'row' ? 'width' : 'height';
+    let oconts = this.el.nativeElement.getBoundingClientRect()[other];
+    this.containerSize = conts;
+    console.log(this.name, 'avi', size, conts, other, oconts)
     window.setTimeout(() => {
-      this.firstPanelSize = ~~(conts / 2);
-      this.secondPanelSize = ~~(conts / 2);
+      this.firstPanelSize = (conts / 2);
+      this.secondPanelSize = (conts / 2);
     });
 
+  }
+  onResize() {
+    let size = this.direction === 'row' ? 'width' : 'height';
+    let conts = this.el.nativeElement.getBoundingClientRect()[size] -
+                this.splitter.nativeElement.getBoundingClientRect()[size];
+      this.firstPanelSize = (conts / 2);
+      this.secondPanelSize = (conts / 2);
   }
   // ngAfterViewChecked()    {
   //   let size = this.direction === 'row' ? 'width' : 'height';
@@ -147,7 +159,7 @@ export class DualPanelComponent implements OnInit, OnDestroy {
   // }
 
   update(e) {
-    console.log(e);
+    // console.log(e);
     let currCoord = e[`client${this.direction === 'row' ? 'X' : 'Y'}`];
     this.firstPanelSize += (currCoord - this.startCoord);
     this.secondPanelSize -= (currCoord - this.startCoord);
