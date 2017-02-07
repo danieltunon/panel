3440bff2a27be8e42ad1a143e6419c27285c377a
Danny Tunon <danieltunon@gmail.com>
2017-02-07 01:01:20 +0000

    Basic resizing working

--- src/app/dual-panel/dual-panel.component.ts
+++ src/app/dual-panel/dual-panel.component.ts
@@ -15,21 +15,28 @@ import { Subject } from 'rxjs/Subject'
 @Component({
   selector: 'dual-panel',
   templateUrl: './dual-panel.component.html',
-  styleUrls: ['./dual-panel.component.less']
+  styleUrls: ['./dual-panel.component.less'],
 })
 export class DualPanelComponent implements OnInit, OnDestroy {
-  @HostBinding('style.flexDirection') @Input() direction: string = 'row';
+  @Input() direction: string = 'row';
+  @Input() containerSize: number;
+  // @HostBinding('style.flexDirection') @Input() direction: string = 'row';
   @Input() name: string;
   @HostListener('mousemove', ['$event']) onmousemove(e: MouseEvent) {
     e.stopPropagation();
-    this.test2$.next(e);
+    // this.test2$.next(e);
+    if (this.isResizing) this.update(e);
+  }
+  @HostListener('mouseup', ['$event']) onmouseup(e: MouseEvent) {
+    this.isResizing = false;
   }
   @ViewChild('splitter') splitter: ElementRef;
 
   private test$: Subject<any> = new Subject();
   private test2$: Subject<any> = new Subject();
 
-  private containerSize: number;
+  private isResizing: boolean = false;
+  startCoord: number;
   firstPanelSize: number = 250;
   secondPanelSize: number = 250;
 
@@ -50,6 +57,7 @@ export class DualPanelComponent implements OnInit, OnDestroy {
   // Maybe break this out into three handlers: start, dragging, and end handlers
   // maybe good use case for observables here {type: startDrag...etc}
   private handleDrag(event: Event): void {
+    console.log(event);
     // if this is a start of drag (event should contain that info?):
       // record the starting size of the container, and the two content children
       // can use getBoundingClientRect()[width/height] - size of the splitter (for the container)
@@ -90,44 +98,66 @@ export class DualPanelComponent implements OnInit, OnDestroy {
   //     this.secondPanelSize = ~~(this.containerSize / 2);
   //   });
   // }
-  ngOnChanges()           {
-    let size = this.direction === 'row' ? 'width' : 'height';
-    let conts = this.el.nativeElement.getBoundingClientRect()[size];
-    console.log(this.name, 'oc', size, conts)
-  }
+  // ngOnChanges()           {
+  //   let size = this.direction === 'row' ? 'width' : 'height';
+  //   let conts = this.el.nativeElement.getBoundingClientRect()[size];
+  //   console.log(this.name, 'oc', size, conts)
+  // }
   ngOnInit()              {
     let size = this.direction === 'row' ? 'width' : 'height';
-    let conts = this.el.nativeElement.getBoundingClientRect()[size];
+    let conts = this.el.nativeElement.getBoundingClientRect()[size] -
+                this.splitter.nativeElement.getBoundingClientRect()[size];
     console.log(this.name, 'oi', size, conts);
+    console.log('bound size', this.containerSize)
     this.firstPanelSize = ~~(conts / 2);
     this.secondPanelSize = ~~(conts / 2);
   }
-  ngDoCheck()             {
-    let size = this.direction === 'row' ? 'width' : 'height';
-    let conts = this.el.nativeElement.getBoundingClientRect()[size];
-    console.log(this.name, 'dc', size, conts)
-  }
-  ngAfterContentInit()    {
-    let size = this.direction === 'row' ? 'width' : 'height';
-    let conts = this.el.nativeElement.getBoundingClientRect()[size];
-    console.log(this.name, 'aci', size, conts)
-  }
-  ngAfterContentChecked() {
-    let size = this.direction === 'row' ? 'width' : 'height';
-    let conts = this.el.nativeElement.getBoundingClientRect()[size];
-    console.log(this.name, 'acc', size, conts);
-    this.firstPanelSize = ~~(conts / 2);
-    this.secondPanelSize = ~~(conts / 2);
-  }
+  // ngDoCheck()             {
+  //   let size = this.direction === 'row' ? 'width' : 'height';
+  //   let conts = this.el.nativeElement.getBoundingClientRect()[size];
+  //   console.log(this.name, 'dc', size, conts)
+  // }
+  // ngAfterContentInit()    {
+  //   let size = this.direction === 'row' ? 'width' : 'height';
+  //   let conts = this.el.nativeElement.getBoundingClientRect()[size];
+  //   console.log(this.name, 'aci', size, conts)
+  // }
+  // ngAfterContentChecked() {
+  //   let size = this.direction === 'row' ? 'width' : 'height';
+  //   let conts = this.el.nativeElement.getBoundingClientRect()[size];
+  //   console.log(this.name, 'acc', size, conts);
+  //   // this.firstPanelSize = ~~(conts / 2);
+  //   // this.secondPanelSize = ~~(conts / 2);
+  // }
   ngAfterViewInit()       {
     let size = this.direction === 'row' ? 'width' : 'height';
-    let conts = this.el.nativeElement.getBoundingClientRect()[size];
+    let conts = this.el.nativeElement.getBoundingClientRect()[size] -
+                this.splitter.nativeElement.getBoundingClientRect()[size];
     console.log(this.name, 'avi', size, conts)
+    window.setTimeout(() => {
+      this.firstPanelSize = ~~(conts / 2);
+      this.secondPanelSize = ~~(conts / 2);
+    });
+
   }
-  ngAfterViewChecked()    {
-    let size = this.direction === 'row' ? 'width' : 'height';
-    let conts = this.el.nativeElement.getBoundingClientRect()[size];
-    console.log(this.name, 'avc', size, conts)
+  // ngAfterViewChecked()    {
+  //   let size = this.direction === 'row' ? 'width' : 'height';
+  //   let conts = ~~this.el.nativeElement.getBoundingClientRect()[size];
+  //   console.log(this.name, 'avc', size, conts)
+
+  // }
+
+  update(e) {
+    console.log(e);
+    let currCoord = e[`client${this.direction === 'row' ? 'X' : 'Y'}`];
+    this.firstPanelSize += (currCoord - this.startCoord);
+    this.secondPanelSize -= (currCoord - this.startCoord);
+    this.startCoord = currCoord;
+  }
+
+  startDrag(e) {
+    this.isResizing = true;
+    this.startCoord = e[`client${this.direction === 'row' ? 'X' : 'Y'}`];
   }
 
 }
