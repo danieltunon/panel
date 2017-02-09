27658bce48dcd0799bda56e8cee2323efcdf298e
Danny Tunon <danieltunon@gmail.com>
2017-02-08 16:13:02 -0800

    Begin refactor to panel and container implementation

--- src/app/panel-container/panel-container.component.ts
+++ src/app/panel-container/panel-container.component.ts
@@ -14,34 +14,34 @@ import
   styleUrls: ['./panel-container.component.less'],
   providers: [],
   host: {
-    '[class]': 'direction'
+    // '[class]': 'direction'
   }
 })
-export class PanelContainerComponent implements AfterContentInit {
-  get containerDimension(): number {
-    return this.direction === 'row'
-      ? this.el.nativeElement.clientWidth
-      : this.el.nativeElement.clientHeight;
-  }
-  el: ElementRef;
-  panelSizes: number[];
+export class PanelContainerComponent {
 
-  @HostBinding('style.flexDirection') @Input() direction = 'row';
-  // @ContentChildren(PanelComponent) panels: QueryList<PanelComponent>;
-  // @ContentChildren(SplitterComponent) splitters: QueryList<SplitterComponent>;
+  constructor() { }
 
-  constructor(el: ElementRef) {
-    this.el = el;
-  }
-  ngOnInit() {
-    console.log(`init size ${this.el.nativeElement.clientWidth}`)
-    console.log(`size is type ${typeof this.el.nativeElement.clientWidth}`)
-  }
-  ngAfterContentInit() {
-    // console.log(this.el.nativeElement.clientWidth)
-    // this.panelSizingService.printMsg();
-    this.panelSizes;
-    // console.log(this.panels.map(p => p.basis))
-  }
+  // get containerDimension(): number {
+  //   return this.direction === 'row'
+  //     ? this.el.nativeElement.clientWidth
+  //     : this.el.nativeElement.clientHeight;
+  // }
+  // el: ElementRef;
+  // panelSizes: number[];
+
+  // @HostBinding('style.flexDirection') @Input() direction = 'row';
+  // // @ContentChildren(PanelComponent) panels: QueryList<PanelComponent>;
+  // // @ContentChildren(SplitterComponent) splitters: QueryList<SplitterComponent>;
+
+  // ngOnInit() {
+  //   console.log(`init size ${this.el.nativeElement.clientWidth}`)
+  //   console.log(`size is type ${typeof this.el.nativeElement.clientWidth}`)
+  // }
+  // ngAfterContentInit() {
+  //   // console.log(this.el.nativeElement.clientWidth)
+  //   // this.panelSizingService.printMsg();
+  //   this.panelSizes;
+  //   // console.log(this.panels.map(p => p.basis))
+  // }
 
 }
