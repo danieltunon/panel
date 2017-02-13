62a6e08f26533956180ed6aac98ba7c6d1a64f2a
Danny Tunon <danieltunon@gmail.com>
2017-02-13 10:38:04 -0800

    Add setFlexBasis method

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'panel',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panel.component.less']
 })
 export class PanelComponent implements OnInit {
+  @HostBinding('style.flexBasis.px') private flexBasis: number;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setFlexBasis(flexBasis: number, containerSize: number): void {
+    let newVal: number = Math.max(0, Math.min(flexBasis, containerSize));
+    if (newVal === 0) newVal = 0.000001;
+    this.flexBasis = newVal;
+    return;
   }
 
 }
