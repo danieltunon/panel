6388fb6bc7471787cfc84dc3e8120da2b8ce0863
Danny Tunon <danieltunon@gmail.com>
2017-02-13 16:30:16 -0800

    Implement resizing logic grossly

--- src/app/panel-container/panel-container.component.ts
+++ src/app/panel-container/panel-container.component.ts
@@ -27,7 +27,9 @@ export class PanelContainerComponent {
   @HostBinding('style.flexDirection') @Input() direction: string = 'row';
   @HostBinding('class.resizing') private isResizing: boolean = false;
   @ContentChildren(PanelComponent) panels: QueryList<PanelComponent>;
-  @ViewChildren('splitter') splitters: QueryList<any>;
+  private panelSizes;
+  @ViewChildren('splitter') splitters: QueryList<ElementRef>;
+  @Input() name: any;
 
   @HostListener('mousemove', ['$event']) onMouseMove(e: MouseEvent) {
     e.preventDefault();
@@ -37,21 +39,32 @@ export class PanelContainerComponent {
   }
   @HostListener('mouseup', ['$event']) endDrag(e: MouseEvent) {
     this.isResizing = false;
-    this.handleResize(e);
   }
 
   private startPosition: number;
+  private activeSplitter: number;
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
-  startDrag(e) {
+  startDrag({e, i}) {
+    e.stopPropagation();
     this.isResizing = true;
+    this.activeSplitter = i;
     this.startPosition = e[`client${this.direction === 'row' ? 'X' : 'Y'}`];
+
   }
 
   handleResize(e) {
+    let dimesion = this.direction === 'row' ? 'width' : 'height';
     let position: number = e[`client${this.direction === 'row' ? 'X' : 'Y'}`];
-    console.log(e);
+    let splitterSize = this.splitters.toArray().reduce((total, s) => total + s.nativeElement.getBoundingClientRect()[dimesion], 0)
+    let containerSize = this.el.nativeElement.getBoundingClientRect()[dimesion] - splitterSize;
+    let panels = this.panels.toArray().slice(this.activeSplitter, this.activeSplitter + 2);
+    console.log(this.name, 'b4', 'panel1', panels[0].flexBasis, 'panel2', panels[1].flexBasis)
+    panels[0].flexBasis += position - this.startPosition;
+    panels[1].flexBasis -= position - this.startPosition;
+    console.log(this.name, 'after', 'panel1', panels[0].flexBasis, 'panel2', panels[1].flexBasis)
+    this.startPosition = position;
   }
 
   // get containerDimension(): number {
@@ -69,10 +82,16 @@ export class PanelContainerComponent {
   // ngOnInit() {
 
   // }
-  ngAfterContentInit() {
+  ngAfterViewInit() {
     // console.log(this.el.nativeElement.clientWidth)
     // this.panelSizingService.printMsg();
     // console.log(this.panels.map(p => p.basis))
+    let dimesion = this.direction === 'row' ? 'width' : 'height';
+    let containerSize = this.el.nativeElement.getBoundingClientRect()[dimesion] -
+                        this.splitters.toArray().reduce((total, s) => total + s.nativeElement.getBoundingClientRect()[dimesion], 0);
+    this.panelSizes = this.panels.toArray().map((p, i, a) => containerSize / a.length)
+    console.log(this.panelSizes)
+    window.setTimeout(() => this.panels.forEach((p, i) => p.flexBasis = this.panelSizes[i]));
   }
 
 }
