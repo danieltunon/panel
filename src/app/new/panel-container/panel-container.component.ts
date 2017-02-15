e7435cacc9920a41438ca2d7e2dee8013f4cf2bc
Danny Tunon <danieltunon@gmail.com>
2017-02-15 14:30:22 -0800

    Resizing working on top level container

--- src/app/new/panel-container/panel-container.component.ts
+++ src/app/new/panel-container/panel-container.component.ts
@@ -28,15 +28,21 @@ import { PanelSizingService, Orientation } from './panel-sizing.service';
 export class PanelContainerComponent {
   private parentContainer: PanelContainerComponent = null;
   @Input() name: string;
-  @HostBinding('class') @Input() orientation: Orientation = Orientation.Vertical;
+  @HostBinding('class') @Input() orientation: string = 'vertical';
   @Input() panelQuantity: number;
   // @ContentChildren(PanelComponent) panels: QueryList<PanelComponent>;
+
+  containerSize: number;
 
   firstFlexBasis: number;
   secondFlexBasis: number;
   thirdFlexBasis: number;
 
-  containerSize: number;
+  targets: Array<string>;
+  isResizing: boolean = false;
+  dimension: string;
+  axis: string;
+  startCoordinate: number;
 
   constructor(
     @Inject('Window') private window: Window,
@@ -54,20 +60,51 @@ export class PanelContainerComponent {
     // if (!this.parentContainer) {
       this.containerSize = this.window.innerWidth - (this.panelQuantity - 1) * 4;
     // }
-    this.firstFlexBasis = this.containerSize / 3;
-    this.secondFlexBasis = this.containerSize / 3;
-    this.thirdFlexBasis = this.containerSize / 3;
+    this.firstFlexBasis = this.containerSize / this.panelQuantity;
+    this.secondFlexBasis = this.containerSize / this.panelQuantity;
+    this.thirdFlexBasis = this.containerSize / this.panelQuantity;
+    switch (this.orientation) {
+      case 'horizontal':
+        this.dimension = 'height';
+        this.axis = 'clientY';
+        break;
+      case 'vertical':
+      default:
+        this.dimension = 'width';
+        this.axis = 'clientX';
+        break;
+    }
   }
 
   ngAfterViewInit() {
     //  window.setTimeout(() => this.sizingService.initializePanelSizes(this.panels.toArray()));
   }
 
-  @HostListener('mousemove', ['$event']) onMouseMove(e: MouseEvent) { this.sizingService.onResize(e) }
-  @HostListener('mouseup', ['$event']) onMouseUp(e: MouseEvent) { this.sizingService.endResize(e) }
-  onMouseDown(e: MouseEvent, splitterIndex: number) {
-    this.sizingService.startResize(e, splitterIndex);
+  @HostListener('mousemove', ['$event']) onMouseMove(e: MouseEvent) { this.onResize(e) }
+  @HostListener('mouseup', ['$event']) onMouseUp(e: MouseEvent) { this.endResize(e) }
+  // onMouseDown(e: MouseEvent, targets: Array<string>) {
+  //   this.startResize(e, targets);
+  // }
+
+  onResize(e: MouseEvent) {
+    e.preventDefault();
+    if (this.isResizing) {
+      let delta: number = e[this.axis] - this.startCoordinate;
+      this[`${this.targets[0]}FlexBasis`] += delta;
+      this[`${this.targets[1]}FlexBasis`] -= delta;
+      this.startCoordinate = e[this.axis]
+
+    }
   }
+  endResize(e: MouseEvent) {
+    this.isResizing = false;
+  }
+  startResize(e: MouseEvent, targets: Array<string>) {
+    this.isResizing = true;
+    this.targets = targets;
+    this.startCoordinate = e[this.axis];
+  }
+
   // @HostListener('mousemove', ['$event']) onMouseMove(e: MouseEvent) { this.sizingService.onResize(e) }
   // @HostListener('mouseup', ['$event']) onMouseUp(e: MouseEvent) { this.sizingService.endResize(e) }
   // onMouseDown(e: MouseEvent, splitterIndex: number) {
