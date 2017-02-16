88ac6254b3fe8a7f8e48794609970c49f5b4d73e
Danny Tunon <danieltunon@gmail.com>
2017-02-16 10:06:38 -0800

    Add order imput to panel - use to select with ng-content

--- src/app/directives/panel.directive.ts
+++ src/app/directives/panel.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[panel]'
 })
-export class PanelDirective {
+export class PanelDirective implements OnInit {
+  @Input('panel') _order: number;
 
   constructor() { }
+
+  ngOnInit() {
+    this._checkOrder();
+  }
+
+  private _checkOrder() {
+    if (!this._order) {
+      throw new Error('Panels must be numbered! Example: <div [panel]="1"></div>');
+    }
+  }
 
 }
