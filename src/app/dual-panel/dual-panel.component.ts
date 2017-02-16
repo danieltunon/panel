86e0a5b19ffbc886499e098e02859a0809686af6
Danny Tunon <danieltunon@gmail.com>
2017-02-16 10:54:08 -0800

    Add orientation input - bind to flex direction

--- src/app/dual-panel/dual-panel.component.ts
+++ src/app/dual-panel/dual-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'dual-panel',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dual-panel.component.less']
 })
 export class DualPanelComponent implements OnInit {
+  @Input() orientation: string;
+  @HostBinding('style.flexDirection') private _flexDirection: string;
 
   constructor() { }
 
   ngOnInit() {
+    this._checkOrientation()
+  }
+
+  private _checkOrientation() {
+    if (this.orientation !== 'vertical' && this.orientation !== 'horizontal') {
+      throw new Error(`Panel container orientation must be either 'vertical' or 'horizontal'. Example: <tri-panel [orientation]="'horizontal'">`);
+    }
+    this._flexDirection = this.orientation === 'vertical' ? 'row' : 'column';
   }
 
 }
--- src/app/tri-panel/tri-panel.component.ts
+++ src/app/tri-panel/tri-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, /*Renderer, ElementRef*/ } from '@angular/core';
 
 @Component({
   selector: 'tri-panel',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tri-panel.component.less']
 })
 export class TriPanelComponent implements OnInit {
+  @Input() orientation: string;
+  @HostBinding('style.flexDirection') private _flexDirection: string;
 
-  constructor() { }
+  constructor(/*private _renderer: Renderer, private _element: ElementRef*/) { }
 
   ngOnInit() {
+    this._checkOrientation()
+  }
+
+  private _checkOrientation() {
+    if (this.orientation !== 'vertical' && this.orientation !== 'horizontal') {
+      throw new Error(`Panel container orientation must be either 'vertical' or 'horizontal'. Example: <tri-panel [orientation]="'horizontal'">`);
+    }
+    this._flexDirection = this.orientation === 'vertical' ? 'row' : 'column';
   }
 
 }
