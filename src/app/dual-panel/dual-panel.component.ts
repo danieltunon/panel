3a697ef507755ee87dc822b9cf93f1c3924a06eb
Danny Tunon <danieltunon@gmail.com>
2017-02-17 07:04:47 -0800

    Set up observable to track resizing

--- src/app/dual-panel/dual-panel.component.ts
+++ src/app/dual-panel/dual-panel.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, HostListener } from '@angular/core';
+import { Subject } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/concatMap';
+import 'rxjs/add/operator/takeUntil';
 
 @Component({
   selector: 'dual-panel',
@@ -9,10 +13,43 @@ export class DualPanelComponent implements OnInit {
   @Input() orientation: string;
   @HostBinding('style.flexDirection') private _flexDirection: string;
 
+  private _axis: string;
+  private _dimension: string;
+
+  startResize$: Subject<any> = new Subject();
+  endResize$: Subject<any> = new Subject();
+  resize$: Subject<any> = new Subject();
+  drag$ = this.startResize$.concatMap( start => {
+    return this.resize$
+      .scan((acc, curr) => ({
+        startPosition: curr[this._axis],
+        delta: curr[this._axis] - acc.startPosition
+      }), {startPosition: start.e[this._axis], delta: 0})
+      .map(r => r.delta)
+      .takeUntil(this.endResize$);
+  });
+
+  @HostListener('mousemove', ['$event']) onMouseMove(e: MouseEvent) { e.preventDefault(); this.resize$.next(e) }
+  @HostListener('mouseup', ['$event']) onMouseUp(e: MouseEvent) { this.endResize$.next(e) }
+  // onMouseDown(e: MouseEvent, targets: Array<ElementRef>) {
+  //   this.startResize$.next({e, targets});
+  // }
+
   constructor() { }
 
   ngOnInit() {
     this._checkOrientation()
+    switch (this.orientation) {
+      case 'horizontal':
+        this._dimension = 'height';
+        this._axis = 'clientY';       
+        break;
+      case 'vertical':
+      default:
+        this._dimension = 'width';
+        this._axis = 'clientX';     
+        break;
+    }
   }
 
   private _checkOrientation() {
